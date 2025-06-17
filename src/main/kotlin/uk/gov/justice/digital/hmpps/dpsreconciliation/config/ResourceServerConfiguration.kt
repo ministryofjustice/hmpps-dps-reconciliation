3346a3a8552fd53157090d37cb537cfaa81014db
steverendell <steve.rendell@syscon.net>
2025-06-17 15:03:50 +0100

    SDIT-2692 Create reconciliation process between release and receive events and Nomis movements part 9

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dpsreconciliation/config/ResourceServerConfiguration.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.dpsreconciliation.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
+
+@Configuration
+class ResourceServerConfiguration {
+  @Bean
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer {
+    unauthorizedRequestPaths {
+      addPaths = setOf(
+        // These endpoints are secured in the ingress rather than the app so that they can be called from within the namespace without requiring authentication
+        "/queue-admin/retry-all-dlqs",
+      )
+    }
+  }
+}
