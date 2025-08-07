f19dc5ced7e18be01ee5446f20a911887404a285
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-08-07 10:49:00 +0100

    SDIT-2094 Try out WebClient changes in latest kotlin lib (#43)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.11")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0-beta")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.10")
