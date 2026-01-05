343e52b04e21617dd67e85d2b0d261f275ece26f
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-01-05 16:51:42 +0000

    SDIT-3270: ⬆️  Use non beta version of hmpps sqs library too (#85)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.0")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.springframework.boot:spring-boot-jackson2")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
